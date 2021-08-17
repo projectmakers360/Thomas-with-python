eb362b8dff948561be7d5003e301346a12180b19
aiml29 <aiml29@abc.com>
2021-08-17 21:38:24 +0530

    aiml29 17-Aug

--- calculus_with_python.py
+++ calculus_with_python.py
@@ -178,7 +178,7 @@ def rng(l,srt = False):
             try:
                 a = l[i] + 1
             except:
-                a = l[i]
+                pass
                 
             if a == l[i+1] :
                 lis_1.append(a)
